641ebbd4a68f3fd6abfb6f6c3ff0d8ced746c5f7
HoaaVu <136317803+HoaaVu@users.noreply.github.com>
2023-06-12 17:29:11 +0700

    Add files via upload

---
+++ Student.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Student(ABC):
+    def __init__(self, name: str, age: int, address: str,):
+        self.name = name
+        self.age = age
+        self.address = address
+
+    def Getname(self):
+        return self.name
+
+    @abstractmethod
+    def SelectDataForPrint(self):
+        pass
+
+    def Print(self):
+        print(self.SelectDataForPrint())
+
+class Test(Student):
+    def SelectDataForPrint(self):
+        return f"Name: {self.name}, Age: {self.age}, Address: {self.address}"
+
+test = Test("Hoa", 22, "654 Lac Long Quan")
+test.Print()
